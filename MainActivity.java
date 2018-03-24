a9a8f4b5020f56f4ef1303e61b2fa4156e56c54c
anilemrah <emrah.anl@gmail.com>
2018-03-24 13:01:08 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.anilemrah.firsttry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RelativeLayout anilslayout = new RelativeLayout(this);
+        anilslayout.setBackgroundColor(Color.GREEN);
+
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText("Log In");
+        redButton.setBackgroundColor(Color.RED);
+
+        //Username Input
+        EditText username = new EditText(this);
+
+        redButton.setId(1);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //Give rules to position widgets
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 250, r.getDisplayMetrics());
+
+        username.setWidth(px);
+
+
+        anilslayout.addView(redButton, buttonDetails);
+        anilslayout.addView(username, usernameDetails);
+        setContentView(anilslayout);
+    }
+}
